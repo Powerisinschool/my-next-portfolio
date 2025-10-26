3081bbef8e001b45765b2fc61e57cd1b78ddfbd4
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2025-10-26 21:09:49 +0100

    feat: add Skills component and integrate into Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 import { Hero } from "@/components/hero";
+import { Skills } from "@/components/skills";
 
 export default function Home() {
   
   return (
     <>
       <Hero />
+      <Skills />
     </>
   );
 }
